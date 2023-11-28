df7531150e016d8843d2ed963c6fffc6c970c78a
anthoncastillo <anthon.hertzum@gmail.com>
2023-11-28 16:41:12 +0100

    Update Bear.java

--- src/ourcode/Organism/OrganismChildren/AnimalChildren/CarnivoreChildren/Bear.java
+++ src/ourcode/Organism/OrganismChildren/AnimalChildren/CarnivoreChildren/Bear.java
@@ -1,7 +1,9 @@
 package ourcode.Organism.OrganismChildren.AnimalChildren.CarnivoreChildren;
+import ourcode.Conditions.Hierarchy;
 import ourcode.Organism.OrganismChildren.AnimalChildren.Carnivore;
 public class Bear extends Carnivore {
 
+    Hierarchy hierarchy = Hierarchy.HIGH;
    public Bear() {
 
     }
