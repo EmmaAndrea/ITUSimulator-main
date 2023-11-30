d480c7709f8550cad874e55cb3f25f42025e2fbc
anthoncastillo <anthon.hertzum@gmail.com>
2023-11-30 18:24:47 +0100

    Made test case for first requirement in burrows

--- src/ourcode/Test/TestBurrow.java
+++ src/ourcode/Test/TestBurrow.java
@@ -1,24 +1,29 @@
 package ourcode.Test;
 
-import itumulator.executable.Program;
 import itumulator.world.World;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ourcode.Setup.InputReader;
+import ourcode.Obstacles.Burrow;
 import ourcode.Setup.ProgramRunner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestBurrow {
     public ProgramRunner programRunner;
     public World world;
 
+    public TestBurrow() {
+        programRunner = new ProgramRunner();
+    }
+
     @BeforeAll
-    public static void setUp(){
+    public static void setUp() {
         System.out.println("Testing for burrows");
     }
 
     @BeforeEach
-    public void startTest(){
+    public void startTest() {
         System.out.println("Test Started");
     }
 
@@ -27,17 +32,37 @@ public class TestBurrow {
      */
     @Test
     public void testConstructor() throws Exception {
-        InputReader input = new InputReader("./data/t1-3a.txt");
-
-        Program p = new Program(input.readWorldSize(), 800, 2000 );
-
-
-        /*
-        programRunner = new ProgramRunner();
         programRunner.create("./data/t1-3a.txt");
-        world = programRunner.getWorld();
-        assertTrue(world.getEntities().containsKey(programRunner.getBurrow()));
-        assertEquals(world.getEntities().size(), 6);
-         */
+        World world = programRunner.getWorld();
+        programRunner.runSimulation(1);
+
+        int burrowCount = 0;
+        for (Object entity : world.getEntities().keySet()) {
+            if (entity instanceof Burrow) {
+                burrowCount++;
+            }
+        }
+
+        assertTrue(burrowCount >= 1 && burrowCount <= 5, "amount of burrow should be between 1 and 5, but was" + burrowCount);
     }
+    /*
+    @Test
+    public void testRabbitStandingOnBurrow() throws Exception {
+        World world = new World(2);
+        IDGenerator id = new IDGenerator();
+
+        Rabbit rabbit = new Rabbit(id);
+        Burrow burrow = new Burrow(id);
+        Burrow burrow1 = new Burrow(id);
+        Burrow burrow2 = new Burrow(id);
+        Burrow burrow3 = new Burrow(id);
+
+
+        rabbit.spawn(world);
+        burrow.spawn(world);
+
+        assertEquals(world.getLocation(rabbit), world.getLocation(burrow));
+    }
+
+     */
 }
