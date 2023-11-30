d41840c14cea187b218c3109c41caa53f22923fb
Alex <atmogens@gmail.com>
2023-11-30 13:06:38 +0100

    added test classes for rabbit and burrow

---
+++ src/ourcode/Test/TestBurrow.java
@@ -0,0 +1,13 @@
+package ourcode.Test;
+
+import ourcode.Setup.ProgramRunner;
+
+public class TestBurrow {
+    ProgramRunner programRunner;
+    /**
+     * This test will provide an instance of a burrow based off of the requirements in 'K1-3a' from 'Tema 1'
+     */
+    public void testContructor() {
+
+    }
+}
