08e2e920e1d4ee0316f483a47517039982657ec4
Emma <emma.ravn7@gmail.com>
2023-11-30 13:24:16 +0100

    Burrow update

--- src/ourcode/Obstacles/Burrow.java
+++ src/ourcode/Obstacles/Burrow.java
@@ -10,40 +10,57 @@ import java.util.List;
 
 /**
  * The Burrow class represents holes and are also the home of rabbits. The Burrow class can add rabbits to live inside
- *  or
+ *  or remove
  */
 
 public class Burrow implements NonBlocking {
-    // list of rabbits which live there
+    // List of rabbits which are currently inside the burrow.
     List<Rabbit> residents;
+
+    // A unique identifier for the burrow.
     int burrow_id;
+
+    // A location for the burrow such that a rabbit can find a way to it.
     Location burrow_location;
 
     /**
-     * the constructor for a Burrow
+     * The constructor for a Burrow.
+     * Makes and ID.
+     * Sets the tile in the world to burrow.
+     * A burrow has its own location.
      */
     public Burrow(int burrow_id, World world, Location location) {
         residents = new ArrayList<>();
         this.burrow_id = burrow_id;
         world.setTile(location, this);
-        burrow_location = location;
+        burrow_location = world.getLocation(this);
     }
 
     /**
-     * method for adding rabbit to list of residents
+     * Adds a given rabbit to the list of residents of the burrow.
      */
-    public void addRabbit(Rabbit rabbit){
+    public void addResident(Rabbit rabbit){
         residents.add(rabbit);
     }
 
     /**
-     * A method for retrieving the ID of a burrow. Returns an integer.
+     * Returns the ID of a burrow.
      */
-    public int getBurrowId() {
+    public int getId() {
         return burrow_id;
     }
 
-    public Location getBurrowLocation() {
+    /**
+     * Returns the location of the burrow.
+     */
+    public Location getLocation() {
         return burrow_location;
+    }
+
+    /**
+     * Returns the list of residents of the burrow.
+     */
+    public List<Rabbit> getResidents() {
+        return residents;
     }
 }
