6104d458337610a410c76ef1376f6a0f45b21f7a
Emma <emma.ravn7@gmail.com>
2023-12-01 14:30:57 +0100

    Fixed rabbit tests and grass tests

--- src/ourcode/Test/Theme1/RabbitTest.java
+++ src/ourcode/Test/Theme1/RabbitTest.java
@@ -33,9 +33,22 @@ public class RabbitTest {
     }
 
     /**
-     * Test if the correct amount of rabbits spawn.
+     * Creates a world based on file "t1-2fg", such that 4 rabbits are spawned.
+     * Checks that entities contains exactly four entities,
+     * Checks a rabbit is in entities
      */
-    @Test
+    @org.testng.annotations.Test
+    public void testRabbitSpawn1() throws Exception{
+        programRunner.create("./data/t1-2fg.txt");
+        world = programRunner.getWorld();
+        programRunner.runSimulation(1);
+        assertEquals(world.getEntities().size(), 4, "4 rabbits should be spawned");
+    }
+
+    /**
+     * Test if the correct amount of rabbits spawn, when file uses a range.
+     */
+    @org.testng.annotations.Test
     public void testRabbitPlacement() throws Exception {
         programRunner.create("./data/t1-2cde.txt");
         world = programRunner.getWorld();
@@ -48,27 +61,28 @@ public class RabbitTest {
             }
         }
 
-        assertTrue(rabbitCount >= 2 && rabbitCount <= 4, "Rabbits spawned should be between 2 and 4, but was " + rabbitCount);
+        assertTrue(rabbitCount >= 2 && rabbitCount <= 4, "between 2 and 4 rabbits should be spawned");
     }
 
     /**
      * Test to see if all rabbits die after expected amount of days without eating.
+     * Spawns 4 rabbits and no grass
      */
-    @Test
+    @org.testng.annotations.Test
     public void testRabbitDeath() throws Exception {
-        programRunner.create("./data/t1-2cde.txt");
+        programRunner.create("./data/t1-2fg.txt");
         world = programRunner.getWorld();
-        Rabbit rabbit = new Rabbit(programRunner.getOriginal_id_generator());
-        rabbit.spawn(world);
+        Rabbit rabbit0 = new Rabbit(programRunner.getOriginal_id_generator());
+        rabbit0.spawn(world);
 
         // Run the simulation for a certain number of steps that would lead to the rabbit's death.
-        programRunner.runSimulation(9); // Adjust the number of steps based on your simulation's logic
+        programRunner.runSimulation(18); // Adjust the number of steps based on your simulation's logic
 
         // After running the simulation, check if the rabbit is removed from the world.
-        assertFalse(world.getEntities().containsKey(rabbit), "Rabbit should be removed from the world after death");
+        assertFalse(world.getEntities().containsKey(rabbit0), "Rabbit should be removed from the world after death");
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void testHungerAfterEat() {
         // Create a 1x1 world
         Program p = new Program(1, 800, 2000);
@@ -96,7 +110,7 @@ public class RabbitTest {
         assertTrue(postFeedingHungerLevel < initialHungerLevel, "Rabbit's hunger should decrease after eating grass");
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void testDeleteAfterEat() {
         // Create a 1x1 world
         Program p = new Program(1, 800, 2000);
@@ -118,40 +132,29 @@ public class RabbitTest {
         assertThrows(Exception.class, () -> assertNull(world.getNonBlocking(world.getLocation(rabbit)), "Grass should be deleted after getting eaten."));
     }
 
-    @Test
-    public void testRabbitBreeding() {
+    @org.testng.annotations.Test
+    public void testRabbitBreeding() throws Exception{
         // Create a world
-        Program p = new Program(5, 800, 100); // Assuming a world size that allows for breeding
-        World world = p.getWorld();
+        programRunner.create("./data/t1-2b.txt");
+        world = programRunner.getWorld();
 
-        IDGenerator idGenerator = new IDGenerator();
-
-        // Spawn rabbits in the world
-        Rabbit rabbit1 = new Rabbit(idGenerator);
-        Rabbit rabbit2 = new Rabbit(idGenerator);
-
-        // Set up the rabbits in suitable locations and conditions for breeding
-        rabbit1.spawn(world);
-        rabbit2.spawn(world);
+        for(int i =0 ; i<3 ; i++){
+            Rabbit rabbit = new Rabbit(programRunner.getOriginal_id_generator());
+            rabbit.spawn(world);
+        }
+        for(int i =0 ; i<25 ; i++){
+            Grass grass = new Grass(programRunner.getOriginal_id_generator());
+            grass.spawn(world);
+        }
 
         // Get the initial count of rabbits
         int initialRabbitCount = countRabbits(world);
 
         // Run the simulation for a number of steps to allow breeding
-        for (int i = 0; i < 10; i++) {
-            p.simulate();
-
-            // Deletes burrows if they make them, we don't want them to go in yet due to bugs.
-            for (Object object : world.getEntities().keySet()) {
-                if (object instanceof Burrow) {
-                    world.delete(object);
-                }
-            }
-        }
+        programRunner.runSimulation(23);
 
         // Get the count of rabbits after running the simulation
         int postSimulationRabbitCount = countRabbits(world);
-        System.out.println(postSimulationRabbitCount);
 
         // Assert that the number of rabbits has increased
         assertTrue(postSimulationRabbitCount > initialRabbitCount, "Rabbits should breed and increase in number");
@@ -167,77 +170,46 @@ public class RabbitTest {
         return counter;
     }
 
-    @Test
-    public void testRabbitsDontBreedThemselves() {
+    @org.testng.annotations.Test
+    public void testRabbitsDontBreedThemselves() throws Exception{
         // Create a world
-        Program p = new Program(5, 800, 100); // Assuming a world size that allows for breeding
-        World world = p.getWorld();
-
-        IDGenerator idGenerator = new IDGenerator();
-
-        // Spawn rabbits in the world
-        Rabbit rabbit1 = new Rabbit(idGenerator);
-
-        // Set up the rabbits in suitable locations and conditions for breeding
-        rabbit1.spawn(world);
+        programRunner.create("./data/t1-2a.txt");
+        world = programRunner.getWorld();
 
         // Get the initial count of rabbits
         int initialRabbitCount = countRabbits(world);
 
         // Run the simulation for a number of steps to allow breeding
-        for (int i = 0; i < 99; i++) {
-            p.simulate();
-
-            // Deletes burrows if they make them, we don't want them to go in yet due to bugs.
-            for (Object object : world.getEntities().keySet()) {
-                if (object instanceof Burrow) {
-                    world.delete(object);
-
-                }
-                if (object instanceof Rabbit) {
-                    Rabbit rabbit = (Rabbit) object;
-                    //rabbit.getBurrow()
-                }
-            }
-        }
+        programRunner.runSimulation(18);
 
         // Get the count of rabbits after running the simulation
         int postSimulationRabbitCount = countRabbits(world);
-        System.out.println(postSimulationRabbitCount);
 
         // Assert that the number of rabbits has increased
         assertTrue(postSimulationRabbitCount == initialRabbitCount, "Rabbits should not breed themselves.");
     }
 
-    /**
-     * Creates a world based on file "t1-2fg", such that 4 rabbits are spawned.
-     * Checks that entities contains exactly four entities,
-     * Checks a rabbit is in entities
-     */
-    @org.testng.annotations.Test
-    public void testRabbit1() throws Exception{
-        programRunner.create("./data/t1-2fg.txt");
-        world = programRunner.getWorld();
-        programRunner.runSimulation(1);
-        assertEquals(world.getEntities().size(), 4);
-        assertTrue(world.getEntities().containsKey(programRunner.getRabbit()));
-    }
+
 
     /**
      * Creates a world based on file "t1-2fg", such that some rabbits and grass are spawned.
      * Spawns one specific rabbit which we track
      * Before the program runs, we check the rabbit is in entities
-     * After the program runs a
+     * After the program runs 100 times, check rabbit is dead
      */
     @org.testng.annotations.Test
-    public void testRabbit2() throws Exception{
-        programRunner.create("./data/t1-2cde.txt");
+    public void testRabbitAgeDeath() throws Exception{
+        programRunner.create("./data/t1-2a.txt");
         world = programRunner.getWorld();
         Rabbit rabbit1 = new Rabbit(programRunner.getOriginal_id_generator());
         rabbit1.spawn(world);
+        for(int i =0 ; i<25 ; i++){
+            Grass grass = new Grass(programRunner.getOriginal_id_generator());
+            grass.spawn(world);
+        }
         assertTrue(world.getEntities().containsKey(rabbit1));
         programRunner.runSimulation(101);
-        assertFalse(world.getEntities().containsKey(rabbit1));
+        assertFalse(world.getEntities().containsKey(rabbit1), "checks rabbit dies of old age");
     }
 
     /**
@@ -245,7 +217,7 @@ public class RabbitTest {
      * Checks that they each make burrows at age 6,
      */
     @org.testng.annotations.Test
-    public void testRabbit3() throws Exception{
+    public void testRabbitsMakeBurrows() throws Exception{
         programRunner.create("./data/t1-2fg.txt");
         world = programRunner.getWorld();
         programRunner.runSimulation(6);
@@ -255,7 +227,7 @@ public class RabbitTest {
                 burrowCount++;
             }
         }
-        assertEquals(4, burrowCount);
+        assertEquals(4, burrowCount, "checks each rabbit creates their own burrow");
     }
 
     @org.testng.annotations.Test
@@ -274,7 +246,7 @@ public class RabbitTest {
                 rabbits_in_burrows++;
             }
         }
-        assertEquals(4, rabbits_in_burrows);
+        assertEquals(4, rabbits_in_burrows, "checks rabbits are in their burrows at night");
     }
 
     @AfterEach
