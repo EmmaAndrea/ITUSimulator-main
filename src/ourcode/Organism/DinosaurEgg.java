5d9a5eee5cf3475bcbe9400e8c9397497f7590d9
Alex <atmogens@gmail.com>
2023-12-11 11:46:03 +0100

    javadoc for footprint and dinosaur egg

--- src/ourcode/Organism/DinosaurEgg.java
+++ src/ourcode/Organism/DinosaurEgg.java
@@ -10,13 +10,31 @@ import ourcode.Setup.IDGenerator;
 
 import java.awt.*;
 
+/**
+ * Represents a dinosaur egg in the simulation environment. This class extends 'Animal' and
+ * implements 'DynamicDisplayInformationProvider' for graphical representation. The egg hatches
+ * into a dinosaur after reaching a certain age.
+ */
 public class DinosaurEgg extends Animal implements DynamicDisplayInformationProvider {
 
+    /**
+     * Constructs a DinosaurEgg with a unique identifier and cordyceps status.
+     * Sets the maximum age after which the egg will hatch into a dinosaur.
+     *
+     * @param id_generator The IDGenerator instance that provides the unique identifier for the egg.
+     * @param has_cordyceps Boolean indicating if the egg is infected with cordyceps.
+     */
     public DinosaurEgg(IDGenerator id_generator, boolean has_cordyceps) {
         super(id_generator, has_cordyceps);
         max_age = 28;
     }
 
+    /**
+     * Defines the behavior of the dinosaur egg in each simulation step. The egg hatches
+     * into a dinosaur upon reaching its maximum age.
+     *
+     * @param world The simulation world in which the egg exists.
+     */
     public void act(World world) {
         super.act(world);
 
@@ -25,6 +43,12 @@ public class DinosaurEgg extends Animal implements DynamicDisplayInformationProv
         }
     }
 
+    /**
+     * Transforms the dinosaur egg into a baby dinosaur. The egg is removed from the world,
+     * and a new dinosaur is created at the same location.
+     *
+     * @param world The simulation world where the transformation occurs.
+     */
     public void becomeDinosaur(World world) {
         Location current_location = world.getLocation(this);
         world.delete(this);
@@ -32,6 +56,12 @@ public class DinosaurEgg extends Animal implements DynamicDisplayInformationProv
         world.setTile(current_location, baby);
     }
 
+    /**
+     * Provides the display information for the dinosaur egg. The appearance changes based on the egg's age,
+     * representing different stages of hatching.
+     *
+     * @return DisplayInformation object containing color and image data for the egg's representation.
+     */
     public DisplayInformation getInformation() {
         if (age < 10) {
             return new DisplayInformation(Color.ORANGE, "egg");
--- src/ourcode/Organism/Footprint.java
+++ src/ourcode/Organism/Footprint.java
@@ -8,24 +8,42 @@ import ourcode.Setup.IDGenerator;
 
 import java.awt.*;
 
+/**
+ * Represents a footprint in the simulation environment. This class extends 'Entity'
+ * and implements 'DynamicDisplayInformationProvider' for graphical representation.
+ * Footprints are created as a trace of an animal's movement and disappear after a certain age.
+ */
 public class Footprint extends Entity implements DynamicDisplayInformationProvider {
 
     /**
-     * Constructor for a footprint, gives them an identity, sets 'age' to 1.
+     * Constructs a Footprint with a unique identifier. Initializes the maximum age
+     * after which the footprint will disappear.
      *
-     * @param original_id_generator The IDGenerator from the program-runner.
+     * @param original_id_generator The IDGenerator instance that provides the unique identifier for the footprint.
      */
     public Footprint(IDGenerator original_id_generator) {
         super(original_id_generator);
         max_age = 30;
     }
 
+    /**
+     * Defines the behavior of the footprint in each simulation step. The footprint is removed
+     * from the world after reaching its maximum age.
+     *
+     * @param world The simulation world in which the footprint exists.
+     */
     public void act(World world) {
         if (age >= max_age) {
             world.remove(this);
         }
     }
 
+    /**
+     * Provides the display information for the footprint. This method defines how the footprint
+     * is visually represented in the simulation.
+     *
+     * @return DisplayInformation object containing color and image data for the footprint's representation.
+     */
     @Override
     public DisplayInformation getInformation() {
         return new DisplayInformation(Color.darkGray, "footprint");
