e89f8d36860cbf6c4064d90a74ed994cda148235
anthoncastillo <anthon.hertzum@gmail.com>
2023-12-12 13:23:59 +0100

    Updated testFungus.java
    Added more tests for Fungus class

--- src/ourcode/Test/Theme3/FungusTest.java
+++ src/ourcode/Test/Theme3/FungusTest.java
@@ -9,11 +9,14 @@ import ourcode.Organism.OrganismChildren.Carcass;
 import ourcode.Organism.OrganismChildren.Fungus;
 import ourcode.Setup.IDGenerator;
 import ourcode.Setup.ProgramRunner;
+import itumulator.executable.Program;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FungusTest {
     public ProgramRunner programRunner;
     public World world;
+    public IDGenerator idGenerator;
 
     public FungusTest() {
         programRunner = new ProgramRunner();
@@ -27,9 +30,6 @@ public class FungusTest {
     @BeforeEach
     public void startTest() throws Exception{
         System.out.println("Test started");
-        programRunner.create("./data/fungitest.txt");
-        world = programRunner.getWorld();
-        programRunner.runSimulation(1); // Run the simulation for one step to spawn entities
     }
 
     /**
@@ -38,16 +38,44 @@ public class FungusTest {
      * @throws Exception
      */
     @Test
-    public void testFungusSpawnBasedOnFile() throws Exception {
-        int fungus_counter = 0;
+    public void testFungusSpawnMethod() throws Exception {
+        world = new World(3);
+        idGenerator = new IDGenerator();
 
-        for (Object object : world.getEntities().keySet()){
-            if (object instanceof Fungus){
-                fungus_counter++;
-            }
-        }
+        Fungus fungus = new Fungus(idGenerator);
+        fungus.spawn(world);
 
-        assertTrue(fungus_counter >= 5 && fungus_counter <= 8, "The amount of fungus should be between 5 and 8, but is: " + fungus_counter);
+        assertEquals(1, world.getEntities().size());
+    }
+
+    @Test
+    public void testFungusCheckCarcassMethod() throws Exception {
+        world = new World(3);
+        idGenerator = new IDGenerator();
+
+        Carcass carcass = new Carcass(idGenerator, 4, "bear", false);
+        carcass.spawn(world);
+        Fungus fungus = new Fungus(idGenerator);
+        fungus.spawn(world);
+        fungus.checkSurroundingCarcass(world);
+
+        assertSame(fungus.checkSurroundingCarcass(world), carcass);
+    }
+
+    /**
+     * This test checks if the spread method from Fungus class is working.
+     */
+    @Test
+    public void testFungusSpreadMethod() {
+        world = new World(3);
+        idGenerator = new IDGenerator();
+
+        Carcass carcass = new Carcass(idGenerator, 4, "bear", false);
+        Fungus fungus = new Fungus(idGenerator);
+        fungus.spawn(world);
+        fungus.spread(carcass);
+
+        assertTrue(carcass.hasFungus());
     }
 
     /**
@@ -56,12 +84,13 @@ public class FungusTest {
      */
     @Test
     public void testFungusCanSpread() throws Exception {
+        programRunner.create("./data/c");
 
         Location base_location = new Location(0,0);
         Carcass new_carcass = new Carcass(programRunner.getOriginal_id_generator(), 3, "rabbit", false);
 
-        for (Location location: world.getSurroundingTiles(base_location, world.getSize())){
-            if (world.isTileEmpty(location)){
+        for (Location location: world.getSurroundingTiles(base_location, world.getSize())) {
+            if (world.isTileEmpty(location)) {
                 world.setTile(location, new_carcass);
                 break;
             }
@@ -71,17 +100,53 @@ public class FungusTest {
         assertTrue(new_carcass.hasFungus());
     }
 
+    @Test
+    public void testFungusSpreadRadius() {
+        Program p = new Program(3,500,800);
+        world = p.getWorld();
+        idGenerator = new IDGenerator();
+
+        Fungus fungus = new Fungus(idGenerator);
+        Carcass carcass = new Carcass(idGenerator,4,"bear",false);
+        Location location_0 = new Location(0,2);
+        Location location_1 = new Location(2,0);
+
+        world.setTile(location_0,fungus);
+        world.setTile(location_1,carcass);
+
+        p.show();
+        for (int i = 0; i < 20; i++) {
+            p.simulate();
+        }
+
+        assertTrue(carcass.hasFungus());
+
+    }
+
 
     /**
      * Tests that no fungus is left in the world after 6 steps
      * Since the only thing in the world is the fungus.
      */
     @Test
-    public void testFungusDiesIfDoesntFindCarcass() {
-
+    public void testFungusDiesIfDoesntFindCarcass() throws Exception {
+        programRunner.create("./data/fungi-test.txt");
+        world = programRunner.getWorld();
         programRunner.runSimulation(6);
 
-        assertTrue(world.getEntities().size() == 0);
+        assertEquals(0, world.getEntities().size());
     }
 
+    @Test
+    public void testFungusSimulation() throws Exception {
+        programRunner.create("./data/test-fungus.txt");
+        world = programRunner.getWorld();
+        programRunner.runSimulation(10);
+
+        for (Object o : world.getEntities().keySet()) {
+            if (o instanceof Carcass) {
+                assertTrue(((Carcass) o).hasFungus());
+            }
+        }
+    }
 }
